56bcab36ad076ee8319b3be9c0dceb2efb097f51
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-12-17 18:56:13 -0300

    Install CORS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('express-async-errors');
 
 const routes = require('./routes');
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
